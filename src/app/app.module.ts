550e089c11885d9e45bfadc98e90e6a9de9c0608
Serghei Luchianenco <s@luchianenco.com>
2018-06-03 22:43:31 +0200

    App Kick-off * created components * material design installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecentlyAddedComponent } from './recently-added/recently-added.component';
+import { SettingsComponent } from './settings/settings.component';
+import { LearnComponent } from './learn/learn.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecentlyAddedComponent,
+    SettingsComponent,
+    LearnComponent
   ],
   imports: [
     BrowserModule
